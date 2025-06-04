c9818d94b8e16cb1b58eb7d3d85d0968f5340455
furkanksl <furkankoseoglu65@gmail.com>
2025-06-04 22:50:59 +0300

    feat: wrap App component with ThemeProvider for dynamic theme support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 import "../styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="system" storageKey="freewhisper-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
